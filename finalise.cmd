d52d932a95f28d4f2b339f37bdcd08c4e65e1536
stevedenner <141518510+stevedenner@users.noreply.github.com>
2023-08-06 18:12:47 +0100

    Update finalise.cmd

--- finalise.cmd
+++ finalise.cmd
@@ -6,7 +6,14 @@ setlocal
 SET /P RESPONSE=This will generalise the image and shut it down ready for adding to the image library.  Are you sure you want to shut down now (Y/[N])?
 IF /I "%RESPONSE%" NEQ "Y" GOTO END
 
-Sysprep /generalize /shutdown
+sc config "wuauserv" start=disabled
+
+Powershell -ExecutionPolicy Unrestricted  "Get-AppxPackage | Remove-AppxPackage"
+
+C:\Windows\System32\Sysprep\Sysprep /generalize /shutdown /oobe /quiet
+
+
 
 :END
 endlocal
+
